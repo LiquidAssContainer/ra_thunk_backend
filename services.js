2d4c17aea24f1211b74575bd4432ec4b1ae035f0
LiquidAssContainer <LiquidAssContainer@users.noreply.github.com>
2021-11-25 00:08:38 +0500

    Change service ids to string type

--- services.js
+++ services.js
@@ -1,24 +1,24 @@
 module.exports = [
   {
-    id: 1,
+    id: '1',
     name: 'Замена стекла',
     price: 21000,
     content: 'Стекло оригинал от Apple',
   },
   {
-    id: 2,
+    id: '2',
     name: 'Замена дисплея',
     price: 25000,
     content: 'Дисплей оригинал от Apple',
   },
   {
-    id: 3,
+    id: '3',
     name: 'Замена аккумулятора',
     price: 4000,
     content: 'Аккумулятор оригинал от Apple',
   },
   {
-    id: 4,
+    id: '4',
     name: 'Замена микрофона',
     price: 2500,
     content: 'Микрофон оригинал от Apple',
